fa26fb5290c678bd1508417f5d21d58c7f7a8d0a
Oybek Artikov <artikov.dev@gmail.com>
2025-04-29 17:46:34 +0500

    change project

--- src/projects.js
+++ src/projects.js
@@ -15,7 +15,7 @@ export const projects =[
     },
     {
         id: 2,
-        project_title: 'Ilhom Makrket',
+        project_title: 'Uchqur',
         project_description: 'Teamed up with a designer to breathe life into a promotional webpage for our beloved show, Adventure Time. Delivered a fully responsive design with dynamic content capabilities, seamlessly integrating a newsletter feature to keep fans updated with the latest adventures.',
         project_info: {
             project_year: 2024,
