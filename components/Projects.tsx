6150b4f81604417c95b46ad3ec0288e788c29f4e
Oybek Artikov <artikov.dev@gmail.com>
2025-05-23 18:00:31 +0500

    Projects section update

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,7 +2,7 @@
 import { projects } from "@/data";
 import React from "react";
 import { PinContainer } from "./ui/3d-pin";
-import Image from "next/image";
+import { FaLocationArrow } from "react-icons/fa6";
 
 const Projects = () => {
 	return (
@@ -11,14 +11,14 @@ const Projects = () => {
 				A small selection of{" "}
 				<span className="text-purple">recent projects</span>
 			</h1>
-			<div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+			<div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
 				{projects.map(({ id, title, des, img, iconLists, link }) => (
 					<div
 						key={id}
-						className="lg:min-h-[32rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+						className="sm:h-[41rem] h-[32rem] lg:min-h-[32rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
 					>
-						<PinContainer title={title} href={link}>
-							<div className="relative flex items-center justify-center sm:w-96 w-[80vh] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+						<PinContainer title={link} href={link}>
+							<div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] sm:h-[40vh] h-[30vh] overflow-hidden mb-10">
 								<div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
 									<img src="/bg.png" alt="bg" />
 									<img
@@ -41,12 +41,18 @@ const Projects = () => {
 									{iconLists.map((icon, index) => (
 										<div
 											key={icon}
-											className="border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex items-center justify-center mr-2"
-											// style={ {`translateX(-${5 * index * 2})`}}
+											className="border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex items-center justify-center"
+											style={{ transform: `translateX(-${5 * index * 2}px)` }}
 										>
 											<img src={icon} alt={icon} className="p-2" />
 										</div>
 									))}
+								</div>
+								<div className="flex justify-center items-center">
+									<p className="flex lg:text-xl md:text-xs text-sm text-purple">
+										Check Live Site
+									</p>
+									<FaLocationArrow className="ms-3" color="#cbacf9" />
 								</div>
 							</div>
 						</PinContainer>
