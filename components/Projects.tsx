5a4bb74d92dc754603ee11bcf81b838ee943038e
Oybek Artikov <oybekartikov@icloud.com>
2025-05-23 21:50:04 +0500

    Projects section done

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,25 +6,29 @@ import { FaLocationArrow } from "react-icons/fa6";
 
 const Projects = () => {
 	return (
-		<div className="py-20">
+		<div className="py-20" id="projects">
 			<h1 className="heading">
 				A small selection of{" "}
 				<span className="text-purple">recent projects</span>
 			</h1>
-			<div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
+			<div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-18 mt-10">
 				{projects.map(({ id, title, des, img, iconLists, link }) => (
 					<div
 						key={id}
 						className="sm:h-[41rem] h-[32rem] lg:min-h-[32rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
 					>
 						<PinContainer title={link} href={link}>
-							<div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] sm:h-[40vh] h-[30vh] overflow-hidden mb-10">
+							<div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] sm:h-[30vh] h-[20vh] overflow-hidden mb-10">
 								<div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
-									<img src="/bg.png" alt="bg" />
+									<img
+										src="/bg.png"
+										alt="bg"
+										className="absolute top-0 left-0 w-full h-full object-cover"
+									/>
 									<img
 										src={img}
 										alt={title}
-										className="z-10 absolute bottom-0"
+										className="z-10 absolute bottom-0 left-1/2 -translate-x-1/2"
 									/>
 								</div>
 							</div>
