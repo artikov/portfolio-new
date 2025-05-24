ec6a645031332d4620e7b3c4c06a9f9114fe3b73
Oybek Artikov <oybekartikov@icloud.com>
2025-05-24 08:22:28 +0500

    Complete Project

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,23 @@ import Grid from "@/components/Grid";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import Clients from "@/components/Clients";
 import Projects from "@/components/Projects";
+import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
 import { navItems } from "@/data";
 
 export default function Home() {
 	return (
-		<main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+		<main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
 			<div className="max-w-7xl w-full">
 				<FloatingNav navItems={navItems} />
 				<Hero />
 				<Grid />
 				<Projects />
 				<Clients />
+				<Experience />
+				<Approach />
+				<Footer />
 			</div>
 		</main>
 	);
